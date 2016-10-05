7eaaac9fddc19210b72715b0c1e6dbcabdbd248a
zhoulindong <zhoulindong@qima-inc.com>
2016-10-05 12:39:10 +0800

    add url encoding

--- controller/url_encoding.go
+++ controller/url_encoding.go
@@ -6,9 +6,9 @@ func BaiduEncoding(domain string) string {
 	argvs := `{"browser":"main","url":"http://` + domain + `","mode":"2"}`
 	urls := url.Values{}
 	urls.Add("command", argvs)
-	return `http://xbox.m.baidu.com/app/share/loop?` + urls.Encode()
+	return `xbox.m.baidu.com/app/share/loop?` + urls.Encode()
 }
 
 func ZhihuEncoding(domain string) string {
-	return `http://link.zhihu.com/?target=http://` + domain
+	return `link.zhihu.com/?target=http://` + domain
 }
